ecdad2acbd909151fcb613cb0486b3e2dc238c8b
root <root@LAPTOP-H141CA1C>
2023-04-08 19:10:43 +0200

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let userInputs=[];
 let funnyStory="The ${userInputs[0]} is a wild ${userInputs[1]}. It can be found in ${userInputs[2]}, ${userInputs[3]} and ${userInputs[4]} color. It has ${userInputs[5]} and a lot of fur on its body. It can ${userInputs[6]} and eat fruits like berries. It loves to ${userInputs[7]} fish and ${userInputs[8]}. ${userInputs[0]} can stand on their two back legs like ${userInputs[9]}."
-
 console.log(funnyStory);
-
+let numbersOfQuestions = [userInputs[0], userInputs[1], userInputs[2], userInputs[3], userInputs[4], userInputs[5], userInputs[6], userInputs[7], userInputs[8], userInputs[9]];
 
 //let originalStory="The bear is a wild animal. It can be found in black, brown and white color. It has sharp teeth and a lot of fur on its body. It can climb trees and eat fruits like berries. It loves to eat fish and honey. Bears can stand on their two back legs like human beings."
