d448a085ee82f23f91aead905ac3020ea3b98e0a
root <root@LAPTOP-H141CA1C>
2023-04-08 20:34:07 +0200

    Finished working on user inputs

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 let userInputs=[];
-let funnyStory="The ${userInputs[0]} is a wild ${userInputs[1]}. It can be found in ${userInputs[2]}, ${userInputs[3]} and ${userInputs[4]} color. It has ${userInputs[5]} and a lot of fur on its body. It can ${userInputs[6]} and eat fruits like berries. It loves to ${userInputs[7]} fish and ${userInputs[8]}. ${userInputs[0]} can stand on their two back legs like ${userInputs[9]}."
-let numbersOfQuestions = [userInputs[0], userInputs[1], userInputs[2], userInputs[3], userInputs[4], userInputs[5], userInputs[6], userInputs[7], userInputs[8], userInputs[9]];
+let funnyStory=`In ${userInputs[0]}, computer pioneer ${userInputs[1]} found themselves working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+let numberOfQuestions = [userInputs[0], userInputs[1], userInputs[2]];
 
-//let noun = prompt("Enter a noun: ");
-//let verb = prompt("Enter a verb");
-//let color = prompt("Enter a color");
-
-let questions = 3;
-let questionArray = ["Enter a noun", "Enter a verb", "Enter a color"];
+let questionArray = ["Enter a year", "Enter a name", "Enter a noun"];
 let questionCounter = 0;
-
-for(let i = questions-1; i >= 0; i--){
-    questions--;
-    console.log(questionArray[questionCounter] + `(${questions} questions left)`);
+let questionTotal = 3;
+for(let i = questionTotal-1; i >= 0; i--){
+    questionTotal--;
+    let skrot = prompt(questionArray[questionCounter] + `(${questionTotal} questions left)`);
+    userInputs.push(skrot);
     questionCounter++;
 };
+alert('All done! Ready for your story?');
 
 //let originalStory="The bear is a wild animal. It can be found in black, brown and white color. It has sharp teeth and a lot of fur on its body. It can climb trees and eat fruits like berries. It loves to eat fish and honey. Bears can stand on their two back legs like human beings."
